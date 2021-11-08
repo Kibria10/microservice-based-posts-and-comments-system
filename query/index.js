9865cf132625cef1d44f7804d44190fac21f9c9f
Kibria10 <junakkibria56@gmail.com>
2021-11-08 16:04:35 +0600

    v0.0.1

--- query/index.js
+++ query/index.js
@@ -5,15 +5,27 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
+const posts = {};
+
 app.get('/posts', (req, res)=>{
-
-
+    res.send(posts);
 });
 
 
 app.post('/events', (req, res)=>{
 
+    if(type == "PostCreated"){
+    const{id, title} = data;
+    post[id] = {id, title, comments: []};
+    }
 
+    if(type == "CommentCreated"){
+        const{id, content, postId} = data;
+        const post = posts[postId];
+        post.comments.push({id, content});
+    }
+
+    res.send({});
 });
 
 

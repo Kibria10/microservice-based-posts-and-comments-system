64f115a021ec80462ff77d3ae25c1a604b272496
Kibria10 <junakkibria56@gmail.com>
2021-11-08 15:27:29 +0600

    v0.0.1

--- posts/index.js
+++ posts/index.js
@@ -1,28 +1,45 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const{randomBytes} = require('crypto');
+const { randomBytes } = require('crypto');
 const cors = require('cors');
+const axios = require('axios');
+
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
+
 const posts = {};
 
 app.get('/posts', (req, res) => {
-    res.send(posts);
+  res.send(posts);
 });
 
-app.post('/posts', (req, res)=> {
-    const id = randomBytes(4).toString('hex');
+app.post('/posts', async (req, res) => {
+  const id = randomBytes(4).toString('hex');
+  const { title } = req.body;
 
-    const {title} = req.body;
+  posts[id] = {
+    id,
+    title
+  };
 
-    posts[id] = {id, title};
+  await axios.post('http://localhost:4005/events', {
+    type: 'PostCreated',
+    data: {
+      id,
+      title
+    }
+  });
 
-    res.status(201).send(posts[id]);
+  res.status(201).send(posts[id]);
+});
+
+app.post('/events', (req, res) => {
+  console.log('Received Event', req.body.type);
+
+  res.send({});
 });
 
 app.listen(4000, () => {
-    console.log('Listening to port 4000');
-}
-);
-
+  console.log('Listening on 4000');
+});

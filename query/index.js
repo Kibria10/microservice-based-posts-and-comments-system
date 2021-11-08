1d41a04f6470fb76f53a7ec69af475a30a6da52a
Kibria10 <junakkibria56@gmail.com>
2021-11-08 15:48:56 +0600

    v0.0.1

---
+++ query/index.js
@@ -0,0 +1,23 @@
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const express = require('express');
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/posts', (req, res)=>{
+
+
+});
+
+
+app.post('/events', (req, res)=>{
+
+
+});
+
+
+app.listen(4002, ()=>{
+console.log('Listening to port 4002');
+
+});

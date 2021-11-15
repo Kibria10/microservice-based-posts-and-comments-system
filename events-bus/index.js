0c46c815d854b4a718521525791da5e5f4f2f7ae
Kibria10 <junakkibria56@gmail.com>
2021-11-15 15:08:21 +0600

    k8s implementation

--- events-bus/index.js
+++ events-bus/index.js
@@ -8,9 +8,10 @@ app.use(bodyParser.json());
 app.post('/events', (req, res) => {
   const event = req.body;
 
-  axios.post('http://localhost:4000/events', event);
-  axios.post('http://localhost:4001/events', event);
-  axios.post('http://localhost:4002/events', event);
+  axios.post('http://posts-clusterip-srv:4000/events', event);
+  axios.post('http://comments-srv:4001/events', event);
+  axios.post('http://query-srv:4002/events', event);
+  // axios.post('http://localhost:4003/events', event); //for moderation 
 
   res.send({ status: 'OK' });
 });
--- posts/index.js
+++ posts/index.js
@@ -25,7 +25,7 @@ try {
     title
   };
 
-  await axios.post('http://localhost:4005/events', {
+  await axios.post('http://events-bus-srv:4005/events', {
     type: 'PostCreated',
     data: {
       id,
